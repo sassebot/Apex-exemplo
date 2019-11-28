6d7e6f35f02a7499b1d47f90d34ae314ad33f767
Felipe Ulloa <sasseulloa@gmail.com>
2019-11-28 19:48:39 -0300

    Mudança no nome

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ola Mundo");
+            Console.WriteLine("Ola carinha");
         }
     }
 }
